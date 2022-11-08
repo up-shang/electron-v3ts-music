4d17557808e1424b24ee0bd2b4ea10b6777308fe
up-shang <943101718@qq.com>
2022-11-08 10:13:01 +0800

    refactor: 重构优化

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
+      "@": path.join(__dirname, "src"), // 相对路径别名配置，使用 @ 代替 src
     },
   },
   server: process.env.VSCODE_DEBUG ? (() => {
